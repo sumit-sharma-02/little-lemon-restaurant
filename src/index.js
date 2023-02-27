1179106d7a9212d309ce6e52738a8c428d2f8c07
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-02-27 14:46:51 +0530

    Update minor UI changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
